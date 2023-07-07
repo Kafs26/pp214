4d37c69dcd3de2f03f89ea2220ae988e364bf944
Kafs26 <s_mih@list.ru>
2023-07-07 22:05:56 +0300

    1stTre

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,27 @@
 package app.config;
 
+
+import app.model.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
+    @Bean (name="dog")
+    public Dog dogBean(){
+        return new Dog();
+    }
+
+    @Bean (name="cat")
+    public Cat catBean(){
+        return new Cat();
+    }
+
+    @Bean (name="timer")
+    public Timer timerBean(){
+        return new Timer();
+    }
 }
